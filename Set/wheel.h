832c962dfcb8d1c53b2d79f83dd83aa0e36ff5f8
Hehe0307 <127603791+Hehe0307@users.noreply.github.com>
2023-12-19 02:21:23 +0800

    Create wheel.h

---
+++ Set/wheel.h
@@ -0,0 +1,19 @@
+#ifndef WHEEL_H_INCLUDED
+#define WHEEL_H_INCLUDED
+
+// Interface class of wheel
+class wheel
+{
+  public:
+    wheel(const int, const int); // Constructor
+    void moveForward(); // For forward movmement
+    void reverse(); // For rotating 180 degree movement 
+    void stop(); // For stopping robot movement
+    virtual void turnLeft() = 0; // To be used in child classes 'leftWheel' and 'rightWheel' 
+    virtual void turnRight() = 0; // To be used in child classes 'leftWheel' and 'rightWheel' 
+  private:
+    const int wheel_pwm; // PWM Pin
+    const int wheel_dir; // Direction Pin
+};
+
+#endif // WHEEL_H_INCLUDED
