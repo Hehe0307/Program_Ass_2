52e5fb77c19035c4a54ee90491c16f73ab11ac2e
damianchong <118364314+damianchong@users.noreply.github.com>
2023-12-29 03:11:40 +0800

    .

---
+++ Assigment_F/Assignment/ultrasonic.h
@@ -0,0 +1,17 @@
+#ifndef ULTRASONIC_H_INCLUDED
+#define ULTRASONIC_H_INCLUDED
+
+// Interface class of ultrasonic
+class ultrasonic
+{
+  public:
+    ultrasonic(const int, const int); // Constructor
+    void declarePin(); // For pin declaration
+    void retrieveData(); // Retrieve sensor readings
+    long data;
+  private:
+    const int trigPin; // Trigger Pin
+    const int echoPin; // Echo Pin
+};
+
+#endif // ULTRASONIC_H_INCLUDED
